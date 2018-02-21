d0e4e5b69250f29191dfba77351f40ce2b969d76
Sebastián Caggiano <seba2610@gmail.com>
2018-02-21 19:16:44 -0300

    Attribute src changed in json file.

--- json.js
+++ json.js
@@ -1,4 +1,4 @@
-[{"nombre":"Marcelo", "apellido":"Rodriguez", "edad":15, "email":"marcelo@ceibal.edu.uy", "src":"http://www.perfectsound.es/wp-content/gallery/equipo/carlos-carnet.jpg"},
-{"nombre":"Catalina", "apellido":"Sanchez", "edad":17, "email":"catalina@ceibal.edu.uy","src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMmUgSIuF4d6qHjY6IzOmF51C32rsF3p2LrY6OLNuedVYhrDF9"},
-{"nombre":"José", "apellido":"Pepe", "edad":14, "email":"jose@ceibal.edu.uy", "src":"http://centromlajoya.webcindario.com/webcam/fotos/foto1.jpg"},
-{"nombre":"Sofía", "apellido":"Sosa", "edad":16, "email":"sofia@ceibal.edu.uy", "src":"https://pbs.twimg.com/profile_images/1895208727/foto_mia_carnet.jpg"}]
+[{"nombre":"Marcelo", "apellido":"Rodriguez", "edad":15, "email":"marcelo@ceibal.edu.uy", "foto":"http://www.perfectsound.es/wp-content/gallery/equipo/carlos-carnet.jpg"},
+{"nombre":"Catalina", "apellido":"Sanchez", "edad":17, "email":"catalina@ceibal.edu.uy","foto":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMmUgSIuF4d6qHjY6IzOmF51C32rsF3p2LrY6OLNuedVYhrDF9"},
+{"nombre":"José", "apellido":"Pepe", "edad":14, "email":"jose@ceibal.edu.uy", "foto":"http://centromlajoya.webcindario.com/webcam/fotos/foto1.jpg"},
+{"nombre":"Sofía", "apellido":"Sosa", "edad":16, "email":"sofia@ceibal.edu.uy", "foto":"https://pbs.twimg.com/profile_images/1895208727/foto_mia_carnet.jpg"}]
