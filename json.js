1e60df97ba81285c1bd6b4cfa6a8dfb05e46b866
Sebastián Caggiano <seba2610@gmail.com>
2018-03-01 17:31:57 -0300

    Image url updated

--- json.js
+++ json.js
@@ -1,4 +1,4 @@
 [{"nombre":"Marcelo", "apellido":"Rodriguez", "edad":15, "email":"marcelo@ceibal.edu.uy", "foto":"http://www.perfectsound.es/wp-content/gallery/equipo/carlos-carnet.jpg"},
 {"nombre":"Catalina", "apellido":"Sanchez", "edad":17, "email":"catalina@ceibal.edu.uy","foto":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMmUgSIuF4d6qHjY6IzOmF51C32rsF3p2LrY6OLNuedVYhrDF9"},
-{"nombre":"José", "apellido":"Pepe", "edad":14, "email":"jose@ceibal.edu.uy", "foto":"http://centromlajoya.webcindario.com/webcam/fotos/foto1.jpg"},
+{"nombre":"José", "apellido":"Pepe", "edad":14, "email":"jose@ceibal.edu.uy", "foto":"http://www.rtve.es/css/rtve.2015.television/rtve.programas/aqui-la-tierra/i/imgaux_small.png"},
 {"nombre":"Sofía", "apellido":"Sosa", "edad":16, "email":"sofia@ceibal.edu.uy", "foto":"https://pbs.twimg.com/profile_images/1895208727/foto_mia_carnet.jpg"}]
